d60d8f8d8517e6a4a79230d4c3dee0689ff76842
Paris Pantigoso <proinnovaperu@outlook.com>
2018-03-25 19:30:15 -0500

    graph and speed result google

--- AbpCompanyName.AbpProjectName.Web.Mvc/Controllers/CrmController.cs
+++ AbpCompanyName.AbpProjectName.Web.Mvc/Controllers/CrmController.cs
@@ -44,92 +44,116 @@ namespace AbpCompanyName.AbpProjectName.Web.Mvc.Controllers
         public JsonResult GetPhoneNumber(string text)
         {
             Response response = new Response();
+            ResultGoogle resultGoogle = new ResultGoogle();
+
+            if (string.IsNullOrEmpty(text))
+            {
+                response.Message = "Texto vacío o nulo";
+                return Json(response);
+            }
 
             try
             {
                 //Get Entities using NLU
-                string resultText = string.Empty;
 
                 string _district = "Lima";
                 string _country = "Perú";
                 string _entity = string.Empty;
 
-                if (text.Length > 14)
+                string _textModified = text.ToLower();
+
+                if (!_textModified.Contains("lima"))
                 {
-                    AnalysisResults _nlu = NluController.Analyze(null, text);
+                    _textModified = _textModified + " Lima";
+                }
 
+                if (!_textModified.Contains("perú") && !_textModified.Contains("peru"))
+                {
+                    _textModified = _textModified + " Perú";
+                }
 
-                    foreach (var item in _nlu.Entities)
+                if (!_textModified.Contains("teléfono") && !_textModified.Contains("telefono"))
+                {
+                    _textModified = "Teléfono " + _textModified;
+                }
+
+                if (_textModified?.Length > 14)
+                {
+                    AnalysisResults _nlu = NluController.Analyze(null, _textModified);
+
+                    if (_nlu.Entities != null)
                     {
-                        switch (item.Type)
+                        foreach (var item in _nlu.Entities)
                         {
-                            case "Organization":
-                                _entity = item.Text;
-                                break;
-                            case "Location":
-                                if (item.Disambiguation.Subtype[0].StartsWith("City"))
-                                {
-                                    _district = item.Text;
-                                }
+                            switch (item.Type)
+                            {
+                                case "Organization":
+                                    _entity = item.Text;
+                                    break;
+                                case "Location":
+                                    if (item.Disambiguation.Subtype[0].StartsWith("City"))
+                                    {
+                                        _district = item.Text;
+                                    }
 
-                                if (item.Disambiguation.Subtype[0].StartsWith("Country"))
-                                {
-                                    _country = item.Text;
-                                }
+                                    if (item.Disambiguation.Subtype[0].StartsWith("Country"))
+                                    {
+                                        _country = item.Text;
+                                    }
 
-                                break;
+                                    break;
 
-                            default:
-                                break;
+                                default:
+                                    break;
+                            }
                         }
+
                     }
 
                     //_entity = _nlu.Entities.FirstOrDefault()?.Text;
 
                     if (string.IsNullOrEmpty(_entity))
                     {
-                        resultText = "No se ha identificado la Entidad. Por favor, vuelva a intentarlo.";
+                        //response.Message = "No se ha identificado la Entidad. Por favor, vuelva a intentarlo.";
+                        //return Json(response);
 
-                        return Json(response);
+                        resultGoogle = GetGoogleSearch(_textModified);
 
                     }
                     else
                     {
                         //string _queryParsed = string.Format("{0} {1} {2}", text, _district, _country);
-                        string _queryParsed = $"teléfono {_entity} {_district} {_country}";
+                        string _queryParsed = $"Teléfono {_entity} {_district} {_country}";
 
-                        resultText = GetGoogleSearch(_queryParsed);
+                        resultGoogle = GetGoogleSearch(_queryParsed);
 
                     }
 
                 }
                 else
                 {
-                    string _textModified = text;
 
-                    if (!_textModified.Contains("Lima"))
-                    {
-                        _textModified = _textModified + " Lima";
-                    }
-
-                    if (!_textModified.Contains("Perú"))
-                    {
-                        _textModified = _textModified + " Perú";
-                    }
-
-                    resultText = GetGoogleSearch(_textModified);
+                    resultGoogle = GetGoogleSearch(_textModified);
 
                 }
 
                 response.Status = true;
 
-                if (Regex.IsMatch(resultText, @"^\d+$"))
+                if (Regex.IsMatch(resultGoogle.Text, @"^\d+$"))
                 {
-                    response.Message = $"El número de teléfono de {_entity} es: {resultText}";
+                    if (string.IsNullOrEmpty(_entity))
+                    {
+                        response.Message = $"El número de teléfono de {resultGoogle.Title} es: {resultGoogle.Text}";
+
+                    }
+                    else
+                    {
+                        response.Message = $"El número de teléfono de {_entity} es: {resultGoogle.Text}";
+                    }
                 }
                 else
                 {
-                    response.Message = resultText;
+                    response.Message = "No se ha encontrado un número. Por favor, vuelva a intentarlo.";
                 }
 
 
@@ -149,42 +173,102 @@ namespace AbpCompanyName.AbpProjectName.Web.Mvc.Controllers
 
         }
 
-        public static string GetGoogleSearch(string query)
+        public static ResultGoogle GetGoogleSearch(string query)
         {
-            string _result = string.Empty;
+            ResultGoogle result = new ResultGoogle();
 
-            var urlGoogle = "http://google.com/search?q=" + query;
+            string urlGoogle = "http://google.com/search?q=" + query;
 
             HtmlWeb web = new HtmlWeb();
 
             HtmlDocument doc = web.Load(urlGoogle);
 
             //the parameter is use xpath see: https://www.w3schools.com/xml/xml_xpath.asp 
+
+            HtmlNodeCollection graphGoogle = doc.DocumentNode.SelectNodes("//table[@class='O6u2Ve']");
+
             HtmlNodeCollection divsGoogle = doc.DocumentNode.SelectNodes("//div[@class='g']");
 
-            foreach (var item in divsGoogle)
+            string _title, _link, _trash, _text = string.Empty;
+            int _length, _index = 0;
+            int? phoneNumber;
+
+            foreach (HtmlNode graph in graphGoogle)
             {
-                if (item.ChildNodes.Count == 2)
+                _title = Helpers.ConvertStringToUTF8(graph.ChildNodes[1].ChildNodes[0].ChildNodes[0].InnerText.Split(",".ToCharArray()).FirstOrDefault());
+                _text = graph.ChildNodes[1].ChildNodes[0].ChildNodes[1].ChildNodes[1].InnerText.Split(";".ToCharArray()).ElementAt(1);
+
+                if (!string.IsNullOrEmpty(_text))
                 {
-                    var _title = Helpers.ConvertStringToUTF8(item.ChildNodes[0].InnerText);
-                    var _link = item.ChildNodes[0].ChildNodes[0].Attributes["href"].Value;
-                    _link = _link.Replace("/url?q=", "");
+                    phoneNumber = GetPhoneNumber(_text, 7, 9);
 
-                    var _length = _link.Length;
-                    var _index = _link.IndexOf("&amp");
+                    if (phoneNumber != null)
+                    {
+                        result.Title = _title.Trim();
+                        result.Text = phoneNumber.ToString();
+                        result.Status = true;
 
-                    var trash = _link.Substring(_index, _length - _index);
-                    _link = _link.Replace(trash, "");
+                        return result;
+                    }
+                }
 
-                    var _text = Helpers.ConvertStringToUTF8(item.ChildNodes[1].ChildNodes[1].InnerText);
+            }
+
+
+            foreach (HtmlNode item in divsGoogle)
+            {
+                if (item.ChildNodes.Count == 1)
+                {
+                    HtmlDocument innerDiv = new HtmlDocument();
+                    innerDiv.LoadHtml(item.InnerHtml.ToString());
+
+                    HtmlNodeCollection divs = innerDiv.DocumentNode.SelectNodes("//div");
+
+                    _title = Helpers.ConvertStringToUTF8(divs[0].ChildNodes[0].ChildNodes[1].InnerText.Split(",".ToCharArray()).FirstOrDefault());
+                    _text = divs[0].ChildNodes[0].ChildNodes[0].InnerText;
 
                     if (!string.IsNullOrEmpty(_text))
                     {
-                        int? phoneNumber = GetPhoneNumber(_text, 7, 9);
+                        phoneNumber = GetPhoneNumber(_text, 7, 9);
 
                         if (phoneNumber != null)
                         {
-                            return phoneNumber.ToString();
+                            result.Title = _title.Trim();
+                            result.Text = phoneNumber.ToString();
+                            result.Status = true;
+
+                            return result;
+                        }
+                    }
+
+                }
+
+                if (item.ChildNodes.Count == 2)
+                {
+                    _title = Helpers.ConvertStringToUTF8(item.ChildNodes[0].InnerText);
+                    _link = item.ChildNodes[0].ChildNodes[0].Attributes["href"].Value;
+                    _link = _link.Replace("/url?q=", "");
+
+                    _length = _link.Length;
+                    _index = _link.IndexOf("&amp");
+
+                    _trash = _link.Substring(_index, _length - _index);
+                    _link = _link.Replace(_trash, "");
+
+                    _text = Helpers.ConvertStringToUTF8(item.ChildNodes[1].ChildNodes[1].InnerText);
+
+                    if (!string.IsNullOrEmpty(_text))
+                    {
+                        phoneNumber = GetPhoneNumber(_text, 7, 9);
+
+                        if (phoneNumber != null)
+                        {
+                            result.Title = _title;
+                            result.Text = phoneNumber.ToString();
+                            result.Link = _link;
+                            result.Status = true;
+
+                            return result;
                         }
                     }
 
@@ -193,9 +277,9 @@ namespace AbpCompanyName.AbpProjectName.Web.Mvc.Controllers
                 }
             }
 
-            _result = "No se ha encontrado el número";
+            result.Text = "No se ha encontrado el número";
 
-            return _result;
+            return result;
         }
 
         internal static int? GetPhoneNumber(string text, int min, int max)
---
+++ AbpCompanyName.AbpProjectName.Web.Mvc/Controllers/ResultGoogle.cs
@@ -0,0 +1,10 @@
+﻿namespace AbpCompanyName.AbpProjectName.Web.Mvc.Controllers
+{
+    public class ResultGoogle
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Link { get; set; }
+        public bool Status { get; set; }
+    }
+}
